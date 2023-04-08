c00eafc0ff2d37084ae2a57cfeb8f596e88f062b
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-08 07:47:53 -0500

    Añadir primeros archivos

---
+++ Parcial1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+/*
+Parcial 1 - Informática II
+Santiago Vargas Higuera
+
+Gestion de informacion para manipulacion
+de horario
+*/
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
