8e709e5d3274eeeaef372b53e3f41440442253f9
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-14 23:40:38 -0500

    Funcion para separar cadena

---
+++ Code/Funciones/split.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+int len_char (char *p);
+void split(char* cadena,char**subcadenas,char sep, int nf);
+int main()
+{
+    char **matrix;
+    char cadena[20] = "22312;Info II;5;4;5";
+    matrix = new char* [5];
+    for(int i = 0; i<5; i++){
+        matrix[i] = new char[10];
+    }
+    split(cadena,matrix,';',5);
+    for(int i = 0;i<5;i++){
+        cout << matrix[i];
+        cout << endl;
+    }
+    for(int i = 0; i<5;i++)delete[] matrix[i];
+    delete[] matrix;
+    return 0;
+}
+int len_char (char *p){
+    int longitud=0;
+    while(p[longitud]!= '\0'){
+        longitud++;
+    }
+    return longitud;
+}
+void split(char* cadena,char**subcadenas,char sep, int nf){
+    //Cadena: Cadena char a "formatear"
+    //Subcadenas: Matriz 2 dimensiones que contiene cada dato como arreglo char
+    //sep: caracter que representa la separación ';' '-' etc
+    //nf: Cantidad de filas que tendrá la matriz.
+    //Por ejemplo, al guardar codigo, nombre, HTI, HTD, nCRED => nf = 5
+    int l = len_char(cadena);
+    int c = 0;
+    for(int i = 0; i<nf;i++){
+        int j = 0;
+        c++;
+        while(cadena[c]!=sep){
+            if(c==l) break;//verifica si recorre toda la cadena
+            subcadenas[i][j]=cadena[c];
+            j++;
+            c++;//copia caracter a caracter en la matriz
+        }
+        subcadenas[i][j]= '\0';
+    }
+
+}
+
+
