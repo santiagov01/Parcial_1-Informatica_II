5280abee30a4a4930ed035f7071e88d8e76e0f30
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-17 18:07:05 -0500

    Validar código. Mostrar mensaje de alerta.

--- Code/Funciones/matricular.cpp
+++ Code/Funciones/matricular.cpp
@@ -20,35 +20,44 @@ void matricular_cursos(char ***Base_datos, char*** Matriculadas, unsigned short 
     [ ] Validar que cantidad sea positiva y menor que el maximo de materias
 
 
-    [ ] Validar que la suma de creditos del estudiante no exceda de 32
+    [X] Validar que la suma de creditos del estudiante no exceda de 32
     [ ] Validar que Curso ingresado no se repita
 
     Hacer una version que dependa solo de creditos
     Hacer una version que dependa de las cantidad y de creditos
 
      * */
-    cout << "Ingresa cantidad de Cursos a Matricular";
-    cin >> *cantidad;
+    //cout << "Ingresa cantidad de Cursos a Matricular";
+    //cin >> *cantidad;
 
     int cred = 0;
     char code_mat[16] = "";
-    int index = 0;//usar para indice
+    int index = 0;//usar como indice de la materia a matricular
+    //se usa para poder acceder al numero de creditos
     int i = 0;//posicion en Matriculadas
-    while(cred<32){//si es con cantidad se hace un bucle for
+    int aux= 0;
+
+    while(cred<=32){//si es con cantidad se hace un bucle for
+        // [ ] Pendiente agregar una función que deje ver los cursos que existen
+        mostrar_informacion(Base_datos,total);
         cout << "Ingresa el codigo de la materia";
         cin >> code_mat;
-        if(validar_materia(Base_datos,code_mat,total,&index)){
-            cred+=char_int(Base_datos[index][4]); //aumenta el numero de creditos actuales
-            if(cred < 32){
-                //vuelve a verificar
-                for(int m = 0; m<len_char(code_mat);m++){
-                    Matriculadas[i][0][m]=code_mat[m];//copia contenido
-                    i++;
+        if((validar_materia(Base_datos,code_mat,total,&index))){
+            //validar que Codigo ingresado existe en la base de datos.
+            if((!validar_materia(Matriculadas,code_mat,i,&aux))){
+                // Validar si Codigo ingresado ya existe
+                cred+=char_int(Base_datos[index][4]); //aumenta el numero de creditos actuales
+                if(cred <=32){
+                    //vuelve a verificar
+                    for(int m = 0; m<len_char(code_mat);m++){
+                        Matriculadas[i][0][m]=code_mat[m];//copia contenido en Matriculados
+                        i++;
+                    }
+                    cantidad++;
                 }
-            }
-        }
+
+            }else cout << "\nError al matricular. La materia " << Base_datos[index][1] << " ya se encuentra matriculada" <<endl;
+        }else cout << "\nError. No se encontró ninguna materia con el codigo ingresado. \nVerifque de nuevo"<<endl;
 
     }
-
-
 }
