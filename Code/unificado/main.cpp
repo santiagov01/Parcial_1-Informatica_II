16dd1cea0b7942cde231120e635cbf737527baa8
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-20 11:57:53 -0500

    Organizar main

--- Code/unificado/main.cpp
+++ Code/unificado/main.cpp
@@ -5,109 +5,49 @@ using namespace std;
 int main()
 {
     char name_file[32]="cursos.txt";
-    int saltos = saltoschar(name_file);
-    int jornada[6];
-    unsigned short int horas_jornada = 0,max_sp=33;
+        int saltos = saltoschar(name_file);
+        int jornada[6];
+        unsigned short int horas_jornada = 0,max_sp=33;
 
-    cout<<"BIENVENIDO AL SISTEMA DE GESTION DE HORARIO"<<endl<<endl;
-    cout<<"Para comenzar creemos su jornada de estudio"<<endl;
-    horas_jornada = crearJornada(jornada);
 
-    char ***calendario = new char**[25];
+        horas_jornada = crearJornada(jornada);
+
+        char ***calendario = new char**[horas_jornada];
 // Asignar MEMORIA------------------------------------------------------------------------
-    char ***Datos = new char **[saltos+1];
-    char ***Matriculadas = new char **[12];//maximo 12 materias
+        char ***Datos = new char **[saltos+1];
+        leer_cursos(Datos,name_file);
+        char ***Matriculadas = new char **[12];//maximo 12 materias
 
-    for (int i = 0; i < 12; i++) {
-      Matriculadas[i] = new char*[10];
-        for (int j = 0; j < 10; j++) {
-            Matriculadas[i][j] = new char[33];
-        }
-    }
-    char dsp[33] = "           Disponible           ";
-    for(int i =0;i< horas_jornada;i++){
-        calendario[i] = new char *[6];
-        for(int j=0;j<6;j++){
-            calendario[i][j] = new char[max_sp];
-        }
-    }
-
-    for(int i =0;i< horas_jornada;i++){
-        for(int j=0;j<6;j++){
-            for(int k= 0;k<max_sp;k++){
-                calendario[i][j][k] = dsp[k];
+        for (int i = 0; i < 12; i++) {
+          Matriculadas[i] = new char*[16];
+            for (int j = 0; j < 16; j++) {
+                Matriculadas[i][j] = new char[10];
             }
         }
-    }
-//------------------------------------------------------------------------
-    unsigned short int num_clases = 0,opcion;
-    leer_cursos(Datos,name_file);
-    do{
-
-        cout<<"Ahora escoga una de las siguientes opciones"<<endl;
-        cout<<"1. Matricular Curso"<<endl;
-        cout<<"2. Ingresar horario de cursos"<<endl;
-        cout<<"3. Ver cursos matriculados"<<endl;
-        cout<<"4. Ingresar horas de trabajo independiente"<<endl;
-        cout<<"5. Mostrar calendario"<<endl;
-        cout<<"0. Salir del programa"<<endl;
-        cin>>opcion;
-
-        switch(opcion){
-        case 1:{
-                matricular_cursos(Datos, Matriculadas, &num_clases,saltos+1);
-                break;
-        }
-
-        case 2:{
-             horario_clases2(Matriculadas, Datos,calendario,num_clases, jornada,saltos+1);
-             break;
-        }
-        case 3:{
-            cout << "\n-----CURSOS MATRICULADOOOS-----" << endl;
-            for(int i = 0;i<num_clases+1;i++){
-                cout << Matriculadas[i][0];
-                cout << " ";
-                cout << endl;
+        char dsp[33] = "           Disponible           ";
+        for(int i =0;i< horas_jornada;i++){
+            calendario[i] = new char *[6];
+            for(int j=0;j<6;j++){
+                calendario[i][j] = new char[max_sp];
             }
-             break;
-
-        }
-        case 4:{
-             rellenar(Datos,calendario,Matriculadas,num_clases, jornada);
-             break;
-
-        }
-        case 5:{
-             mostrarCalendario(jornada,calendario);
-             break;
-
-        }
-        default: cout<<"ingrese una opcion valida"<<endl;
         }
 
+        for(int i =0;i< horas_jornada;i++){
+            for(int j=0;j<6;j++){
+                for(int k= 0;k<max_sp;k++){
+                    calendario[i][j][k] = dsp[k];
+                }
+            }
+        }
 
-
-       }while(opcion!=0);
+//-----------MENU---------------------------------------------------------
+    menu(Datos,Matriculadas,calendario,jornada,saltos);
 
 //--------------------------LIBERAR MEMORIA-----------------------------
-    for(int i = 0; i<12;i++){
-        for(int j = 0; j<10; j++){
-            delete[] Matriculadas[i][j];
-        }
-        delete[] Matriculadas[i];
-    }
-    delete[] Matriculadas;
 
-    for(int i = 0; i<horas_jornada;i++){
-        for(int j = 0; j<6; j++){
-            delete[] calendario[i][j];
-        }
-        delete[] calendario[i];
-    }
-    delete[] calendario;
-
-    liberar_memoria(Datos,saltos);
+    liberar_memoria(Matriculadas,12,16);
+    liberar_memoria(calendario,horas_jornada,6);
+    liberar_memoria(Datos,saltos+1,5);
     return 0;
 
 }
