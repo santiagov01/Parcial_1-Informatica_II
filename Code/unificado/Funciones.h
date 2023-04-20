22eef47f6e80e17fd62ca6f44047955fa715a170
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-20 11:58:24 -0500

    Organizar funciones

--- Code/unificado/Funciones.h
+++ Code/unificado/Funciones.h
@@ -10,7 +10,7 @@ int dec_10(int a);
 void split(char* cadena,char**subcadenas,char sep, int nf);
 bool compare(char a[], char b[]);
 int saltoschar(char name_file[]);
-void liberar_memoria(char***, int saltos);
+void liberar_memoria(char*** matrix, int filas, int columnas);
 
 void beauty(char *nombre,char *copia_bonita,int l_final);
 void mostrar_informacion(char*** Base_datos,int total);
@@ -19,13 +19,15 @@ void mostrarCalendario(int *jornada, char ***calendario);
 int crearJornada(int *jornada);
 void leer_cursos(char ***cursos,char[]);
 
+void validar_tipo_hora(int *hora);
 void validar_dia(char*** datos, int *index, char dia_letra, unsigned short *dia);
-void validar_hora(char*** datos, int * jornada,short int *indice_hora, int index,unsigned short int hora);
+void validar_hora(char*** datos, int * jornada, short int *indice_hora, int index, int hora);
 void matricular_cursos(char ***Base_datos, char*** Matriculadas, unsigned short int *cantidad,int total);
 bool validar_materia(char*** Base_datos, char* codigo,int total, int*);
 int indice_dia2(char matriculados);
 void horario_clases2(char ***Matriculados, char ***datos, char ***calendario, int num_clases , int *jornada,int saltos);
 void reservarHora2(char ***calendario, int dia, int hora,char ***datos,int index );
+void menu(char***Datos, char***Matriculadas, char***calendario, int jornada[], int saltos);
 
 void reservarIndep(char ***calendario, int dia, int hora,char ***datos,int index );
 void formato_indpendientes(char *nombre_materia,char *copia_indp,int l_final);
